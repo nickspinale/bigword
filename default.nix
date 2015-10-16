a531eab7b5ee80873ead5eb6d431167899d77c46
Nick Spinale <spinalen@carleton.edu>
2015-10-16 12:17:42 -0500

    working on new idea

--- default.nix
+++ default.nix
@@ -1,12 +1,12 @@
 { mkDerivation, base, binary, bytestring, HUnit, mod-n, QuickCheck
 , stdenv, test-framework, test-framework-hunit
-, test-framework-quickcheck2
+, test-framework-quickcheck2, data-type-util
 }:
 mkDerivation {
   pname = "bigword";
   version = "0.1.0.0";
   src = ./.;
-  libraryHaskellDepends = [ base mod-n ];
+  libraryHaskellDepends = [ base mod-n data-type-util ];
   testHaskellDepends = [
     base binary bytestring HUnit QuickCheck test-framework
     test-framework-hunit test-framework-quickcheck2
