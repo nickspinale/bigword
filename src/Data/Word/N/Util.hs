1476d3f665a36f160b8ef2916ab5fcf35367ad78
Nick Spinale <spinalen@carleton.edu>
2015-10-16 23:33:17 -0500

    something kinda works

--- src/Data/Word/N/Util.hs
+++ src/Data/Word/N/Util.hs
@@ -1,19 +1,17 @@
 {-# LANGUAGE DataKinds                  #-}
-{-# LANGUAGE DeriveDataTypeable         #-}
 {-# LANGUAGE FlexibleContexts           #-}
 {-# LANGUAGE FlexibleInstances          #-}
-{-# LANGUAGE GeneralizedNewtypeDeriving #-}
 {-# LANGUAGE KindSignatures             #-}
 {-# LANGUAGE MultiParamTypeClasses      #-}
 {-# LANGUAGE Rank2Types                 #-}
 {-# LANGUAGE RankNTypes                 #-}
 {-# LANGUAGE ScopedTypeVariables        #-}
-{-# LANGUAGE StandaloneDeriving         #-}
 {-# LANGUAGE TypeFamilies               #-}
 {-# LANGUAGE TypeOperators              #-}
-{-# LANGUAGE MagicHash                  #-}
 {-# LANGUAGE FunctionalDependencies     #-}
 {-# LANGUAGE PolyKinds                  #-}
+{-# LANGUAGE UndecidableInstances       #-}
+-- {-# LANGUAGE IncoherentInstances       #-}
 -- {-# LANGUAGE AllowAmbiguousTypes        #-}
 
 ---------------------------------------------------------
@@ -35,68 +33,26 @@ module Data.Word.N.Util
 
 import Control.Applicative
 import Data.Word.N
--- import Data.Type.Function
+import Data.Type.List
 import Data.Proxy
 import Data.Traversable
 import Data.Type.Equality
 import Data.Type.Bool
 import GHC.TypeLits
 
--- type Fn xs res = Foldr (->) res xs
+type Fn list result = Foldr (->) result list
 
--- churchify :: forall (n :: Nat) (list :: [Nat]) (result :: *).
---              (n ~ Sum list) => W n -> Fn (Map W list) result -> result
--- churchify = undefined
+class Church list sum | list -> sum where
+    inspect :: Proxy list -> Fn (Map W list) result -> W sum -> result
 
--- class Church (list :: [Nat]) where
---     type View list result :: *
---     inspect :: View list result -> W (Sum list) -> result
+instance Church '[n] n where
+    inspect = const ($)
 
--- instance Church (n ': '[]) where
---     type View (n ': '[]) result = W n -> result
---     inspect = id
-
--- instance Church (m ': os) => Church (n ': m ': os) where
---     type View (n ': m ': os) result = W n -> View (m ': os) result
---     inspect v w = inspect (v high) low
---       where
---         (high, low) = split w
-
-
-
--- class Church (list :: [Nat]) where
---     type Fun list result :: *
---     type Sum list :: Nat
---     inspect :: (m ~ Sum list) => Fun list result -> W m -> result
-
--- instance Church '[] where
---     type Fun '[] result = result
---     type Sum '[] = 0
---     inspect = const
-
--- -- instance (View (n ': ns) result ~ View (n ': ns) result, Church ns) => Church (n ': ns) where
--- instance Church ns => Church (n ': ns) where
---     type Fun (n ': ns) result = W n -> Fun ns result
---     type Sum (n ': ns) = n + Sum ns
---     inspect v w = inspect (v high) low
---       where
---         high :: W n
---         low :: W (Sum ns)
---         (high, low) = split w
-
-class Church f sum result | f -> sum, f -> result where
-    inspect :: W sum -> f -> result
-
-instance Church (W n -> result) n result where
-    inspect = flip ($)
-
-instance ( Triplet m n o
-         , Church f m result
-         ) => Church (W n -> f) o result where
-    inspect w f = inspect low $ f high
+instance (Triplet nos m mnos, Church (n ': os) nos) => Church (m ': n ': os) mnos where
+    inspect _ f w = inspect (Proxy :: Proxy (n ': os)) (f high) low 
       where
-        high :: W n
-        low :: W m
+        high :: W m
+        low :: W nos
         (high, low) = split w
 
 subj :: W 2
@@ -106,26 +62,4 @@ look :: W 1 -> W 1 -> String
 look x y = show x ++ show y
 
 go :: String
-go = inspect subj look
-
--- instance Church '[] 0 (W 0 -> a) where
---     inspect _ f = f
-
--- type family Id (a :: *) :: * where
---     Id a = a
-
--- -- -- instance (View (n ': ns) result ~ View (n ': ns) result, Church ns) => Church (n ': ns) where
--- instance ( Church ns oldSum oldFunction
---          , sum ~ (n + oldSum)
---          , function ~ (W n -> oldFunction)
---          ) => Church (n ': ns) sum function where
---     inspect = undefined
-
--- class Folding (list :: [a]) where
---     fold :: forall (f :: a -> *) comb. (forall n. f n) -> (forall m n. f m -> f n -> f (comb m n)) -> Fn (Map f list) (f (Foldr1 comb list))
-
--- instance Folding ('[x] :: [a]) where
---     fold sing _ = sing
-
--- class KnownList (a :: [b]) where
-    -- knowList :: b -> (
+go = inspect (Proxy :: Proxy '[1, 1]) look subj 
