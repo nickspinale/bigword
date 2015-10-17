560cc9a53ca7f6c07df429ee9aa7d30be4e45fd2
Nick Spinale <spinalen@carleton.edu>
2015-10-17 16:55:47 -0500

    ghc crash!

--- src/Data/Word/N/Util.hs
+++ src/Data/Word/N/Util.hs
@@ -1,5 +1,6 @@
 {-# LANGUAGE DataKinds                  #-}
 {-# LANGUAGE FlexibleInstances          #-}
+{-# LANGUAGE FlexibleContexts           #-}
 {-# LANGUAGE KindSignatures             #-}
 {-# LANGUAGE MultiParamTypeClasses      #-}
 {-# LANGUAGE ScopedTypeVariables        #-}
@@ -26,6 +27,8 @@ module Data.Word.N.Util
     ) where
 
 import Data.Word.N
+import Control.Applicative
+import Data.Functor.Identity
 
 import Data.Function
 import Data.Proxy
@@ -35,63 +38,31 @@ import GHC.TypeLits
 
 -- | Type synonym for readability. Constructs a the type of functions from
 -- the pattern of @'W'@'s specified by 'list' to 'result'.
--- type Fn list result = Foldr (->) result (Map W list)
+type Fn list result = Foldr (->) result (Map W list)
 
-class Functor fn => Church (head ::  Nat  )
-                           (tail :: [Nat] )
-                           (sum  ::  Nat  )
-                           (fn   :: * -> *) | fn -> head
-                                            , fn -> tail
-                                            , fn -> sum where
+class Church list _ f => Flatten f a fa where
 
-    inspect :: Proxy (head ': tail) -> fn result -> W sum -> result
-    construct :: Proxy (head ': tail) -> fn (W sum)
+-- instance Flatten Identity 
+
+class Functor f => Church (list :: [Nat] )
+                          (sum  ::  Nat  )
+                          (f    :: * -> *) | list -> sum, list -> f where
+
+    inspect :: Proxy list -> f result -> W sum -> result
+    construct :: Triplet n sum newSum => Proxy list -> W n -> f (W newSum)
+    -- construct :: Proxy (head ': tail) -> fn (W sum)
     -- construct :: Proxy list -> Fn list (W (Sum list))
 
-instance Church head '[] head ((->) (W head)) where
-    inspect = flip const
-    construct = flip const
+instance Church '[] 0 Identity where
+    inspect _ f _ = runIdentity f
+    construct _ = Identity
 
-instance ( Triplet head sum sum'
-         , Church x xs sum fn
-         ) => Church head (x ': xs) sum' (Compose ((->) (W head)) fn) where
-
-    inspect _ f w = inspect (Proxy :: Proxy (x ': xs)) (getCompose f high) low 
+instance ( Triplet x sum sum'
+         , Church list sum f
+         ) => Church (x ': list) sum' (Compose ((->) (W x)) f) where
+    inspect _ f w = inspect (Proxy :: Proxy list) (getCompose f high) low
       where
-        high :: W head
+        high :: W x
         low :: W sum
         (high, low) = split w
-
-    construct _ = Compose $ \high -> fmap ((high :: W head) >+<) (construct (Proxy :: Proxy (x ': xs)))
-
--- instance ( Triplet Church x xs => Church head (x ': xs) where
---     inspect _ f w = inspect (Proxy :: Proxy '(x, xs)) (f high) low 
---       where
---         (high, low) = split w
-
--- class Church head tail where
---     inspect :: Proxy '(head, tail) -> (W head -> Fn tail result) -> W (head + Sum tail) -> result
---     -- construct :: Proxy list -> Fn list (W (Sum list))
-
--- instance Church head '[] where
---     inspect _ f w = f w
---     -- construct = flip const
-
--- instance ( Triplet Church x xs => Church head (x ': xs) where
---     inspect _ f w = inspect (Proxy :: Proxy '(x, xs)) (f high) low 
---       where
---         (high, low) = split w
-
--- instance Church (y ': zs) => Church (x ': y ': zs) where
--- instance ( Triplet x (Sum (y ': zs)) (Sum (x ': y ': zs))
---          , Church (y ': zs)
---          ) => Church (x ': y ': zs) where
--- instance ( Triplet x (Sum 
---          , Church (y ': zs)
---          ) => Church head (x ': xs) where
-
---     inspect _ f w = inspect (Proxy :: Proxy (y ': zs)) (f high) low 
---       where
---         (high, low) = split w
-
---     construct _ = \high -> ((high :: W x) >+<) . (construct (Proxy :: Proxy (y ': zs)) :: Fn (y ': zs) (W (Sum (y ': zs))))
+    construct _ high = Compose $ (fmap . fmap) (high >+<) (construct (Proxy :: Proxy list))
