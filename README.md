c07eec87b6e2a50505af8ae6daf6eb8293f6c5c7
Nick Spinale <spinalen@carleton.edu>
2015-10-17 19:31:42 -0500

    wut

--- README.md
+++ README.md
@@ -8,3 +8,7 @@ Fixed-size bit-vectors, built over the mod-n package.
 
 *   Better solution for `:|:` typeclass (possibly [this](https://ghc.haskell.org/trac/ghc/wiki/TypeNats/MatchingOnNats))
 *   Generalize the members of the `:|:` typeclass
+
+### TODO
+
+*   Report ghc bug (see tag ghcbug)
--- src/Data/Word/N/Util.hs
+++ src/Data/Word/N/Util.hs
@@ -8,6 +8,10 @@
 {-# LANGUAGE TypeOperators              #-}
 {-# LANGUAGE UndecidableInstances       #-}
 {-# LANGUAGE FunctionalDependencies     #-}
+{-# LANGUAGE InstanceSigs               #-}
+{-# LANGUAGE RankNTypes                 #-}
+{-# LANGUAGE ConstraintKinds            #-}
+{-# LANGUAGE PolyKinds                  #-}
 
 ---------------------------------------------------------
 -- |
@@ -36,35 +40,82 @@ import Data.Type.List
 import Data.Functor.Compose
 import GHC.TypeLits
 
+-- inspect :: forall n ns. Church ns => Proxy
+
 -- | Type synonym for readability. Constructs a the type of functions from
 -- the pattern of @'W'@'s specified by 'list' to 'result'.
 type Fn list result = Foldr (->) result (Map W list)
 
-type family Flatten a where
-    Flatten (Identity b) = b
-    Flatten (Compose c d) = c (Flatten d)
+newtype Fun list result = Fun { unFun :: Fn list result }
+
+type ListSum n ns = Triplet n (Sum ns) (n + Sum ns)
+
+-- type family Flatten a where
+--     Flatten (Identity b) = b
+--     Flatten (Compose c d) = c (Flatten d)
 
 -- instance Flatten Identity 
 
-class Functor f => Church (list :: [Nat] )
-                          (sum  ::  Nat  )
-                          (f    :: * -> *) | list -> sum, list -> f where
+class ListSum head tail => Church (head :: Nat) (tail :: [Nat]) where
+    construct :: Proxy head -> Proxy tail -> W head -> Fun tail (W (head + Sum tail))
+    inspect :: Proxy head -> Proxy tail -> (W head -> Fun tail result) -> W (head + Sum tail) -> result
 
-    inspect :: Proxy list -> f result -> W sum -> result
-    construct :: Triplet n sum newSum => Proxy list -> W n -> f (W newSum)
-    -- construct :: Proxy (head ': tail) -> fn (W sum)
-    -- construct :: Proxy list -> Fn list (W (Sum list))
+instance ListSum head '[] => Church head '[] where
+    construct _ _ = unFun
+    inspect _ _ = unFun
+--     -- construct _ high = high
+--     -- inspect _ f w = f w
 
-instance Church '[] 0 Identity where
-    inspect _ f _ = runIdentity f
-    construct _ = Identity
+-- -- type family UnProxy a where
+-- --     UnProxy a = W a
 
-instance ( Triplet x sum sum'
-         , Church list sum f
-         ) => Church (x ': list) sum' (Compose ((->) (W x)) f) where
-    inspect _ f w = inspect (Proxy :: Proxy list) (getCompose f high) low
-      where
-        high :: W x
-        low :: W sum
-        (high, low) = split w
-    construct _ high = Compose $ (fmap . fmap) (high >+<) (construct (Proxy :: Proxy list))
+-- instance (ListSum head (n ': ns), Church n ns) => Church head (n ': ns) where
+--     -- construct :: ListSum high (n ': ns) => Proxy high
+--     --                                     -> Proxy (n ': ns)
+--     --                                     -> W high
+--     --                                     -> Fn (n ': ns) (W (high + Sum (n ': ns)))
+--     -- construct proxy _ high = ((high :: W high) >+<) . construct (Proxy :: Proxy n) (Proxy :: Proxy ns)
+--     construct _ _ head = ((head :: W head) >+<)
+--                        . (construct (Proxy :: Proxy n) (Proxy :: Proxy ns))
+
+--     inspect _ _ f w =
+--         let (head, low) = split w
+--         in inspect (Proxy :: Proxy n) (Proxy :: Proxy ns) (f head) low
+-- class ListSum head tail => Church (head :: Nat) (tail :: [Nat]) where
+
+--     construct :: Proxy head -> Proxy tail -> W head -> Fn tail (W (head + Sum tail))
+
+--     inspect :: Proxy head -> Proxy tail -> (W head -> Fn tail result) -> W (head + Sum tail) -> result
+
+-- instance ListSum head '[] => Church head '[] where
+--     construct _ _ = id
+--     inspect _ _ = id
+--     -- construct _ high = high
+--     -- inspect _ f w = f w
+
+-- -- type family UnProxy a where
+-- --     UnProxy a = W a
+
+-- instance (ListSum high (n ': ns), Church n ns) => Church high (n ': ns) where
+--     -- construct :: ListSum high (n ': ns) => Proxy high
+--     --                                     -> Proxy (n ': ns)
+--     --                                     -> W high
+--     --                                     -> Fn (n ': ns) (W (high + Sum (n ': ns)))
+--     -- construct proxy _ high = ((high :: W high) >+<) . construct (Proxy :: Proxy n) (Proxy :: Proxy ns)
+--     construct _ _ high = ((high :: W high) >+<)
+--                        . (construct (Proxy :: Proxy n) (Proxy :: Proxy ns)
+--                             :: W n -> Fn (n ': ns) (W (n + Sum ns)))
+
+--     inspect _ _ f w =
+--         let (high, low) = split w
+--         in inspect (Proxy :: Proxy n) (Proxy :: Proxy ns) (f high) low
+
+-- instance ( Triplet x sum sum'
+--          , Church list sum f
+--          ) => Church (x ': list) sum' (Compose ((->) (W x)) f) where
+--     inspect _ f w = inspect (Proxy :: Proxy list) (getCompose f high) low
+--       where
+--         high :: W x
+--         low :: W sum
+--         (high, low) = split w
+--     construct _ high = Compose $ (fmap . fmap) (high >+<) (construct (Proxy :: Proxy list))
